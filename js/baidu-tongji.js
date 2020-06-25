a5a215139459b0d90c6c9d02ccff3ba2f3814f08
BrainPhD-CN <BrainPhD_CN@126.com>
2020-06-25 22:37:21 +0800

    Deployed 5240199 with MkDocs version: 1.1.2

---
+++ js/baidu-tongji.js
@@ -0,0 +1,9 @@
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?0cab768a0cd3ad8ba05fffdcc9d56004";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
